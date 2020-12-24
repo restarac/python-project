6a6785058976b05c1d15bc9140a8fa0edffe4d42
Frederico Pereira <frpereira@uolinc.com>
2020-12-24 17:01:58 -0300

    Creating the MADLIB

---
+++ madlib/main.py
@@ -0,0 +1,22 @@
+# print("How operation works, 1 + 1:")
+# print(1 + 1)
+# print(0.1 + 0.2) # This is weird... https://0.30000000000000004.com/
+# print(.2 + .3)
+# print(float("1") + 0.3)
+
+
+# print("how interpolations works:")
+# string_value = "concatenation"
+# print("This is a " + string_value)
+# print("This is a {}".format(string_value))
+# print(f"This is a {string_value}") # this is likable
+
+# adj1 = input("Adjective: ")
+# verb1 = input("Verb: ")
+# noun1 = input("Noum: ")
+# madlib = f"Studying is so {adj1}! This is because we can't {verb1} as we want. Besides you must think \
+#     about how this {noun1} affect your life."
+
+from samples import phrases
+
+print(phrases.compound_phrases())
---
+++ madlib/samples/phrases.py
@@ -0,0 +1,22 @@
+from samples import samples
+# https://www.thoughtco.com/phrase-grammar-1691625
+#  Types of Phrases With Examples
+#     Noun Phrase
+#     "Buy a big bright green pleasure machine!" — Paul Simon, "The Big Bright Green Pleasure Machine," 1966
+#     Verb Phrase
+#     "Your father may be going away for a little while." — Ellen Griswold in the movie "Vacation," 1983
+#     Adjective Phrase
+#     "It is always the best policy to speak the truth—unless, of course, you are an exceptionally good liar." — Jerome K. Jerome, "The Idler," February 1892
+#     Adverb Phrase
+#     "Movements born in hatred very quickly take on the characteristics of the thing they oppose." — J. S. Habgood, "The Observer," May 4, 1986
+#     Prepositional Phrase
+#     "I could dance with you till the cows come home. On second thought, I'd rather dance with the cows till you come home." —Groucho Marx in "Duck Soup," 1933
+
+def noum_phrases():
+    return f'{samples.random_verb()} {samples.random_quantitative()} {samples.random_noun()} for {samples.random_pronouns()}'
+
+def verb_phrases():
+    return f'{samples.random_pronouns()} {samples.random_noun()} may {samples.random_verb()} {samples.random_quantitative()}'
+
+def compound_phrases():
+    return f'{noum_phrases()} because {verb_phrases()}.'
