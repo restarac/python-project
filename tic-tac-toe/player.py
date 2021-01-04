fbb37fc0b0ce62e56726f0dcb961b7f96e22c415
Frederico Pereira <frpereira@uolinc.com>
2021-01-04 17:48:47 -0300

    Adds the tic-tac-toe

---
+++ tic-tac-toe/player.py
@@ -0,0 +1,41 @@
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+
+    def get_move(self, game):
+        """
+        we want all player get their next move given a game
+        """
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        return super().__init__(letter)
+
+    def get_move(self, game):
+        # get a valid spot
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        return super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we are going to check the values
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again')
+
+        return val
